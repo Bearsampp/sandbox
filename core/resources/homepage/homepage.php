fb1c86ef1f581db054ab293b61f1efde0129ee52
Bear <programming@hallhome.us>
2024-05-02 17:41:02 -0500

    Refactor homepage.php to use dynamic resource paths and improve meta tags

--- core/resources/homepage/homepage.php
+++ core/resources/homepage/homepage.php
@@ -1,35 +1,54 @@
-<?php include __DIR__ . '/../../root.php'; ?>
+<?php
+include __DIR__ . '/../../root.php';
+$resourcesPath = $bearsamppHomepage->getResourcesPath();
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset = "utf-8">
-    <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-    <meta name = "description" content = "">
-    <meta name = "author" content = "">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Localhost Dashboard">
+    <meta name="author" content="Bearsampp">
 
-    <link href = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/css/app.css" rel = "stylesheet">
-    <link href = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/bootstrap/bootstrap.min.css" rel = "stylesheet">
-    <link href = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/fontawesome/css/all.css" rel = "stylesheet">
-    <link href = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/fontawesome/css/v4-shims.css" rel = "stylesheet">
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/jquery/jquery-3.7.1.min.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/jquery/jquery-migrate-3.4.0.min.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/bootstrap/popper.min.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/bootstrap/bootstrap.min.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/fontawesome/js/all.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/libs/fontawesome/js/v4-shims.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/_commons.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/latestversion.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/summary.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/apache.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/filezilla.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/mailhog.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/mariadb.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/memcached.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/mysql.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/nodejs.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/php.js"></script>
-    <script src = "<?php echo $bearsamppHomepage->getResourcesPath(); ?>/js/postgresql.js"></script>
-    <link href = "<?php echo Util::imgToBase64( $bearsamppCore->getResourcesPath() . '/bearsampp.ico' ); ?>" rel = "icon" />
+    <?php
+    $cssFiles = [
+        "/css/app.css",
+        "/libs/bootstrap/bootstrap.min.css",
+        "/libs/fontawesome/css/all.css",
+        "/libs/fontawesome/css/v4-shims.css"
+    ];
+    $jsFiles = [
+        "/libs/jquery/jquery-3.7.1.min.js",
+        "/libs/jquery/jquery-migrate-3.4.0.min.js",
+        "/libs/bootstrap/popper.min.js",
+        "/libs/bootstrap/bootstrap.min.js",
+        "/libs/fontawesome/js/all.js",
+        "/libs/fontawesome/js/v4-shims.js",
+        "/js/_commons.js",
+        "/js/latestversion.js",
+        "/js/summary.js",
+        "/js/apache.js",
+        "/js/filezilla.js",
+        "/js/mailhog.js",
+        "/js/mariadb.js",
+        "/js/memcached.js",
+        "/js/mysql.js",
+        "/js/nodejs.js",
+        "/js/php.js",
+        "/js/postgresql.js"
+    ];
+
+    foreach ($cssFiles as $file) {
+        echo '<link href="' . $resourcesPath . $file . '" rel="stylesheet">' . PHP_EOL;
+    }
+
+    foreach ($jsFiles as $file) {
+        echo '<script src="' . $resourcesPath . $file . '"></script>' . PHP_EOL;
+    }
+    ?>
+
+    <link href="<?php echo Util::imgToBase64($bearsamppCore->getResourcesPath() . '/bearsampp.ico'); ?>" rel="icon" />
     <title><?php echo APP_TITLE . ' ' . $bearsamppCore->getAppVersion(); ?></title>
 </head>
 
@@ -39,7 +58,7 @@
         <div class = "d-inline-block">
             <a class = "navbar-brand" href = "<?php echo Util::getWebsiteUrl(); ?>">
                 <img class = "p-1" alt = "<?php echo APP_TITLE . ' ' . $bearsamppCore->getAppVersion(); ?>"
-                     src = "<?php echo $bearsamppHomepage->getResourcesPath() . '/img/header-logo.png'; ?>" /></a>
+                     src = "<?php echo $resourcesPath . '/img/header-logo.png'; ?>" /></a>
             <button class = "navbar-toggler" type = "button" data-bs-toggle = "collapse" data-bs-target = "#navbarSupportedContent" aria-controls = "navbarSupportedContent"
                     aria-expanded = "false" aria-label = "Toggle navigation">
                 <span class = "navbar-toggler-icon"></span>
@@ -51,12 +70,12 @@
             <li>
                 <a data-bs-toggle = "tooltip" data-bs-placement = "top" data-bs-title = "<?php echo $bearsamppLang->getValue( Lang::GITHUB ); ?>" target = "_blank"
                    href = "<?php echo Util::getGithubUrl(); ?>"><img
-                        src = "<?php echo $bearsamppHomepage->getResourcesPath() . '/img/github.png'; ?>" /></a>
+                        src = "<?php echo $resourcesPath . '/img/github.png'; ?>" /></a>
             </li>
             <li>
                 <a data-bs-toggle = "tooltip" data-bs-placement = "top" data-bs-title = "<?php echo $bearsamppLang->getValue( Lang::DONATE ); ?>" target = "_blank"
                    href = "<?php echo Util::getWebsiteUrl( 'donate' ); ?>"><img
-                        src = "<?php echo $bearsamppHomepage->getResourcesPath() . '/img/heart.png'; ?>" /></a>
+                        src = "<?php echo $resourcesPath . '/img/heart.png'; ?>" /></a>
             </li>
         </ul>
     </div>
