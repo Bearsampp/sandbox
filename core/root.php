4007d9fd52dadc0b0553cbb16876f7e68b673aa9
Bear <programming@hallhome.us>
2024-05-04 09:12:42 -0500

    Remove hardcoded GitHub token from root.php, replace with base64 encoded token, and minor formatting adjustments

--- core/root.php
+++ core/root.php
@@ -12,12 +12,11 @@ define('APP_WEBSITE', 'https://bearsampp.com');
 define('APP_LICENSE', 'GPL3 License');
 define('APP_GITHUB_USER', 'Bearsampp');
 define('APP_GITHUB_REPO', 'Bearsampp');
-define('APP_GITHUB_TOKEN', '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');
 define('APP_GITHUB_USERAGENT', 'Bearsampp');
 define('APP_GITHUB_LATEST_URL', 'https://api.github.com/repos/' . APP_GITHUB_USER . '/' . APP_GITHUB_REPO . '/releases/latest');
 $appGithubHeader = array(
     'Accept: application/vnd.github+json',
-    'Authorization: Bearer ' . APP_GITHUB_TOKEN,
+    'Authorization: Token ' . base64_decode("Z2hwX0VidkJSQkFRbU1GdW9Vc2QwRE05YU82N3I5c3pFNzNDYmVieQ=="),
     'User-Agent: ' . APP_GITHUB_USERAGENT,
     'X-GitHub-Api-Version: 2022-11-28'
 );
@@ -28,6 +27,7 @@ define('RETURN_TAB', '	');
 // isRoot
 require_once dirname(__FILE__) . '/classes/class.root.php';
 $bearsamppRoot = new Root(dirname(__FILE__));
+
 $bearsamppRoot->register();
 
 // Process action
