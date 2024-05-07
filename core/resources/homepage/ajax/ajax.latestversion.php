8c88e0e9f75513224783da5babdd88fbe20e0297
Bear <programming@hallhome.us>
2024-05-07 13:34:58 -0500

    better docblocks and comments

--- core/resources/homepage/ajax/ajax.latestversion.php
+++ core/resources/homepage/ajax/ajax.latestversion.php
@@ -5,6 +5,21 @@
  * Website: https://bearsampp.com
  * Github: https://github.com/Bearsampp
  */
+
+/**
+ * Constructs the download link for a new version if available.
+ *
+ * This function compares the current application version with the latest available version.
+ * If the latest version is greater, it sets the display flag to true and constructs an HTML link
+ * for downloading the new version, appending it to the 'download' key in the result array.
+ *
+ * @global string $bearsamppCurrentVersion The current version of the application.
+ * @global string $bearsamppLatestVersion The latest available version of the application.
+ * @global string $latestVersionUrl The URL to download the latest version.
+ * @global object $bearsamppLang Language management object, used for retrieving language-specific values.
+ * @param array $result Holds the display flag and download link HTML.
+ * @return void Modifies the $result array by reference.
+ */
 global $bearsamppLang, $bearsamppCore;
 
 $result = array(
@@ -16,15 +31,26 @@ $result = array(
 $bearsamppCurrentVersion = $bearsamppCore->getAppVersion();
 
 // Assuming getLatestVersion now returns an array with version and URL
-$latestVersionData = Util::getLatestVersion( APP_GITHUB_LATEST_URL);
+$latestVersionData = Util::getLatestVersion( APP_GITHUB_LATEST_URL );
 
+/* Strip array into individual relevant strings */
 $bearsamppLatestVersion = $latestVersionData['version'];
 $latestVersionUrl       = $latestVersionData['url']; // URL of the latest version
 
+/* These lines are used to create DateTime objects from version strings formatted as 'Year.Month.Day'. */
 $currentVersionDate = DateTime::createFromFormat( 'Y.n.j', $bearsamppCurrentVersion );
 $latestVersionDate  = DateTime::createFromFormat( 'Y.n.j', $bearsamppLatestVersion );
 
-if ( $latestVersionData != null && version_compare( $bearsamppCurrentVersion, $bearsamppLatestVersion, '<' ) )
+/* check to see if everything went sideways */
+if ( $latestVersionData === null )
+{
+    Util::logError( 'version check returned crickets' );
+
+    return;
+}
+
+/* Now that we have all our data create the banner for localhost showing that there is an update */
+if ( version_compare( $bearsamppCurrentVersion, $bearsamppLatestVersion, '<' ) )
 {
     $result['display']  = true;
     $result['download'] .= '<a role="button" class="btn btn-success fullversionurl" href="' . $latestVersionUrl . '" target="_blank"><i class="fa fa-download"></i> ';
