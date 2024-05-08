f4885e04b600a406ab0e9ff327f5479c032b3e1f
Bear <programming@hallhome.us>
2024-05-07 22:45:32 -0500

    somehow copyright got deleted too

--- core/root.php
+++ core/root.php
@@ -5,7 +5,9 @@
  * Website: https://bearsampp.com
  * Github: https://github.com/Bearsampp
  */
-
+/**
+ * Defines constants used throughout the Bearsampp application.
+ */
 define('APP_AUTHOR_NAME', '/N6REJ');
 define('APP_TITLE', 'Bearsampp');
 define('APP_WEBSITE', 'https://bearsampp.com');
@@ -16,22 +18,30 @@ define('APP_GITHUB_USERAGENT', 'Bearsampp');
 define('APP_GITHUB_LATEST_URL', 'https://api.github.com/repos/' . APP_GITHUB_USER . '/' . APP_GITHUB_REPO . '/releases/latest');
 define('RETURN_TAB', '	');
 
-
-// isRoot
+/**
+ * Includes the Root class file and creates an instance of Root.
+ * Registers the root directory of the application.
+ */
 require_once dirname(__FILE__) . '/classes/class.root.php';
 $bearsamppRoot = new Root(dirname(__FILE__));
-
 $bearsamppRoot->register();
 
-// Process action
+/**
+ * Creates an instance of the Action class and processes the action based on command line arguments.
+ */
 $bearsamppAction = new Action();
 $bearsamppAction->process();
 
-// Stop loading
+/**
+ * Checks if the current user has root privileges and stops loading if true.
+ */
 if ($bearsamppRoot->isRoot()) {
     Util::stopLoading();
 }
 
-// Get language locale for the webpage
+/**
+ * Creates an instance of the LangProc class to handle language-specific settings.
+ * Retrieves the locale setting from the language data.
+ */
 $langProc = new LangProc();
 $locale = $langProc->getValue('locale');
