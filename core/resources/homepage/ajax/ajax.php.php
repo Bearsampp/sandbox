f6accaed5afc76960ba0036b32ff3c2d4c78d3bd
Jacob Waisner <jwaisner@users.noreply.github.com>
2024-05-11 13:45:47 -0500

    Revert "homepage fixes"

--- core/resources/homepage/ajax/ajax.php.php
+++ core/resources/homepage/ajax/ajax.php.php
@@ -40,9 +40,9 @@ $result['pearversion'] .= '<span class="float-right badge text-bg-primary">' . $
 // Extensions list
 foreach ($bearsamppBins->getPhp()->getExtensionsFromConf() as $extName => $extStatus) {
     if ($extStatus == ActionSwitchPhpExtension::SWITCH_ON) {
-        $result['extslist'] .= '<span class="span-grid col-xs-12 col-md-2"><i class="fa-regular fa-circle-check"></i> <strong>' . $extName . ' <sup>' . phpversion(substr($extName, 4)) . '</sup></strong></span>';
+        $result['extslist'] .= '<span class="span-grid col-xs-12 col-md-2"><i class="fa fa-check-square-o"></i> <strong>' . $extName . ' <sup>' . phpversion(substr($extName, 4)) . '</sup></strong></span>';
     } else {
-        $result['extslist'] .= '<span class="span-grid col-xs-12 col-md-2"><i class="fa-regular fa-circle"></i> ' . $extName . '</span>';
+        $result['extslist'] .= '<span class="span-grid col-xs-12 col-md-2"><i class="fa fa-square-o"></i> ' . $extName . '</span>';
     }
 }
 
