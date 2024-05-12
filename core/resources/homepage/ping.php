500a746e820fb47ac82a7b4364663531ffbf7fd5
Bear <programming@hallhome.us>
2024-05-11 20:30:42 -0500

    updated all copyrights Update copyright year and add author tag across various PHP classes and actions, enhance documentation links and functionality in about section, and refine functionality in database root password change action.

--- core/resources/homepage/ping.php
+++ core/resources/homepage/ping.php
@@ -1,1 +1,8 @@
 <?php echo 'Pong!';
+/*
+ * Copyright (c) 2024 Bearsampp
+ * License:  GNU General Public License version 3 or later; see LICENSE.txt
+ * Author: @author@
+ * Website: https://bearsampp.com
+ * Github: https://github.com/Bearsampp
+ */
