63099dc6f20fcfe0a1dcc8eba60e1ae10211db92
Bear <programming@hallhome.us>
2024-05-13 11:49:21 -0500

    Refactor codebase for consistency and clarity, update comments and documentation, and remove unnecessary ellipses in log messages and echo statements.

--- core/classes/class.autoloader.php
+++ core/classes/class.autoloader.php
@@ -7,12 +7,28 @@
  * Github: https://github.com/Bearsampp
  */
 
+/**
+ * Autoloader class for dynamically loading classes based on their names.
+ * This class handles the inclusion of class files from specific directories
+ * based on naming conventions.
+ */
 class Autoloader
 {
+    /**
+     * Constructs the Autoloader object.
+     */
     public function __construct()
     {
     }
 
+    /**
+     * Attempts to load a class file based on the class name.
+     * The method modifies the class name to match the expected file path and name,
+     * then includes the file if it exists.
+     *
+     * @param string $class The name of the class to load.
+     * @return bool Returns true if the file exists and was included, false otherwise.
+     */
     public function load($class)
     {
         global $bearsamppRoot;
@@ -49,11 +65,21 @@ class Autoloader
         return true;
     }
 
+    /**
+     * Registers this Autoloader instance on the SPL autoload stack.
+     *
+     * @return bool Returns true on success, false on failure.
+     */
     public function register()
     {
         return spl_autoload_register(array($this, 'load'));
     }
 
+    /**
+     * Unregisters this Autoloader instance from the SPL autoload stack.
+     *
+     * @return bool Returns true on success, false on failure.
+     */
     public function unregister()
     {
         return spl_autoload_unregister(array($this, 'load'));
