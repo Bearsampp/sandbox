3b77940829852b1689c9257194f21bbb04af6f68
Bear <programming@hallhome.us>
2024-05-16 17:56:53 -0500

    Delete core directory
    doing hard reset on core

--- core/resources/homepage/tpls/hp.latestversion.php
+++
@@ -1,5 +0,0 @@
-<div class="alert alert-dismissable alert-success no-display latestversion">
-    <button data-dismiss="alert" class="close" type="button"><i class = 'text-primary fa-solid fa-circle-info'></i></button>
-    <h4><?php echo $bearsamppLang->getValue(Lang::CHECK_VERSION_AVAILABLE_TEXT); ?></h4>
-    <div class="latestversion-download"></div>
-</div>
