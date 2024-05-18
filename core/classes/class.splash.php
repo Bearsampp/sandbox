def125800578c455d5c12281cf66f35eb40de4bc
Bear <programming@hallhome.us>
2024-05-18 07:35:45 -0500

    Adjust splash screen progress bar width in class.splash.php

--- core/classes/class.splash.php
+++ core/classes/class.splash.php
@@ -41,7 +41,7 @@ class Splash
 
         $this->wbWindow = $bearsamppWinbinder->createWindow(null, ToolDialog, $title, $xPos, $yPos, self::WINDOW_WIDTH, self::WINDOW_HEIGHT, WBC_TOP | WBC_READONLY, null);
         $this->wbImage = $bearsamppWinbinder->drawImage($this->wbWindow, $bearsamppCore->getResourcesPath() . '/icons/app.ico');
-        $this->wbProgressBar = $bearsamppWinbinder->createProgressBar($this->wbWindow, $gauge + 1, 42, 24, 390, 15);
+        $this->wbProgressBar = $bearsamppWinbinder->createProgressBar($this->wbWindow, $gauge + 1, 42, 24, 380, 15);
 
         $this->setTextLoading($text);
         $this->incrProgressBar();
