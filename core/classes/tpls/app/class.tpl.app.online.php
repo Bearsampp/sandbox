cb7c2d9439787287f059eb70a222448fdeab54dd
Bear <programming@hallhome.us>
2024-05-21 05:28:35 -0500

    Refactor class.util.php and class.tpl.service.php with updated copyright headers, documentation comments, and enhanced service management functionalities

--- core/classes/tpls/app/class.tpl.app.online.php
+++ core/classes/tpls/app/class.tpl.app.online.php
@@ -1,20 +1,27 @@
 <?php
+/*
+ * Copyright (c) 2021-2024 Bearsampp
+ * License:  GNU General Public License version 3 or later; see LICENSE.txt
+ * Author: Bear
+ * Website: https://bearsampp.com
+ * Github: https://github.com/Bearsampp
+ */
 
 class TplAppOnline
 {
     const ACTION = 'status';
-    
+
     public static function process()
     {
         global $bearsamppConfig, $bearsamppLang;
-        
+
         return TplApp::getActionMulti(
             self::ACTION, array($bearsamppConfig->isOnline() ? Config::DISABLED : Config::ENABLED),
             array($bearsamppConfig->isOnline() ? $bearsamppLang->getValue(Lang::MENU_PUT_OFFLINE) : $bearsamppLang->getValue(Lang::MENU_PUT_ONLINE)),
             false, get_called_class()
         );
     }
-    
+
     public static function getActionStatus($status)
     {
         return TplApp::getActionRun(Action::SWITCH_ONLINE, array($status)) . PHP_EOL .
--- core/resources/homepage/js/latestversion.js
+++ core/resources/homepage/js/latestversion.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2021-2024 Bearsampp
+ * License:  GNU General Public License version 3 or later; see LICENSE.txt
+ * Author: Bear
+ * Website: https://bearsampp.com
+ * Github: https://github.com/Bearsampp
+ */
+
 /*$(document).ready(function() {
   if ($('.latestversion').length) {
     $.ajax({
@@ -53,4 +61,3 @@ document.addEventListener("DOMContentLoaded", function() {
     getLatestVersionStatus();
   }
 })
-
