daadd888491a7fe3e24294733c6394aeb26f15db
Bear <programming@hallhome.us>
2024-05-23 08:47:04 -0500

    Add comprehensive documentation for PHP, MySQL, and MariaDB modules in Bearsampp, enhance class structures with detailed docblocks, and update configuration management for various services including Node.js and Apache.

--- core/classes/bins/class.bins.php
+++ core/classes/bins/class.bins.php
@@ -7,25 +7,75 @@
  * Github: https://github.com/Bearsampp
  */
 
+/**
+ * Class Bins manages various service binaries like Mailhog, Memcached, Apache, etc.
+ * It provides methods to reload and update configurations for these services.
+ */
 class Bins
 {
+    /**
+     * Constant to define the type of bins.
+     */
     const TYPE = 'bins';
 
+    /**
+     * @var BinMailhog|null Instance of Mailhog binary handler.
+     */
     private $mailhog;
+
+    /**
+     * @var BinMemcached|null Instance of Memcached binary handler.
+     */
     private $memcached;
+
+    /**
+     * @var BinApache|null Instance of Apache binary handler.
+     */
     private $apache;
+
+    /**
+     * @var BinPhp|null Instance of PHP binary handler.
+     */
     private $php;
+
+    /**
+     * @var BinMysql|null Instance of MySQL binary handler.
+     */
     private $mysql;
+
+    /**
+     * @var BinMariadb|null Instance of MariaDB binary handler.
+     */
     private $mariadb;
+
+    /**
+     * @var BinPostgresql|null Instance of PostgreSQL binary handler.
+     */
     private $postgresql;
+
+    /**
+     * @var BinNodejs|null Instance of Node.js binary handler.
+     */
     private $nodejs;
+
+    /**
+     * @var BinFilezilla|null Instance of Filezilla binary handler.
+     */
     private $filezilla;
 
+    /**
+     * Constructor for Bins class.
+     * Initializes the class and logs the initialization.
+     */
     public function __construct()
     {
         Util::logInitClass($this);
     }
 
+    /**
+     * Reloads all service configurations.
+     * Iterates through all service handlers and triggers their reload method.
+     */
     public function reload()
     {
         Util::logInfo('Reload bins');
@@ -34,6 +84,10 @@ class Bins
         }
     }
 
+    /**
+     * Updates all service configurations.
+     * Iterates through all service handlers and triggers their update method.
+     */
     public function update()
     {
         Util::logInfo('Update bins config');
@@ -42,6 +96,11 @@ class Bins
         }
     }
 
+    /**
+     * Retrieves all service handlers as an array.
+     *
+     * @return array An array of all service handlers.
+     */
     public function getAll()
     {
         return array(
@@ -56,7 +115,12 @@ class Bins
             $this->getNodejs(),
         );
     }
-
+    /**
+     * Retrieves the Mailhog binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinMailhog.
+     *
+     * @return BinMailhog The Mailhog binary handler instance.
+     */
     public function getMailhog()
     {
         if ($this->mailhog == null) {
@@ -65,6 +129,12 @@ class Bins
         return $this->mailhog;
     }
 
+    /**
+     * Retrieves the Memcached binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinMemcached.
+     *
+     * @return BinMemcached The Memcached binary handler instance.
+     */
     public function getMemcached()
     {
         if ($this->memcached == null) {
@@ -73,6 +143,12 @@ class Bins
         return $this->memcached;
     }
 
+    /**
+     * Retrieves the Apache binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinApache.
+     *
+     * @return BinApache The Apache binary handler instance.
+     */
     public function getApache()
     {
         if ($this->apache == null) {
@@ -81,6 +157,12 @@ class Bins
         return $this->apache;
     }
 
+    /**
+     * Retrieves the PHP binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinPhp.
+     *
+     * @return BinPhp The PHP binary handler instance.
+     */
     public function getPhp()
     {
         if ($this->php == null) {
@@ -89,6 +171,12 @@ class Bins
         return $this->php;
     }
 
+    /**
+     * Retrieves the MySQL binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinMysql.
+     *
+     * @return BinMysql The MySQL binary handler instance.
+     */
     public function getMysql()
     {
         if ($this->mysql == null) {
@@ -97,6 +185,12 @@ class Bins
         return $this->mysql;
     }
 
+    /**
+     * Retrieves the MariaDB binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinMariadb.
+     *
+     * @return BinMariadb The MariaDB binary handler instance.
+     */
     public function getMariadb()
     {
         if ($this->mariadb == null) {
@@ -105,6 +199,12 @@ class Bins
         return $this->mariadb;
     }
 
+    /**
+     * Retrieves the PostgreSQL binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinPostgresql.
+     *
+     * @return BinPostgresql The PostgreSQL binary handler instance.
+     */
     public function getPostgresql()
     {
         if ($this->postgresql == null) {
@@ -113,6 +213,12 @@ class Bins
         return $this->postgresql;
     }
 
+    /**
+     * Retrieves the Node.js binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinNodejs.
+     *
+     * @return BinNodejs The Node.js binary handler instance.
+     */
     public function getNodejs()
     {
         if ($this->nodejs == null) {
@@ -121,6 +227,12 @@ class Bins
         return $this->nodejs;
     }
 
+    /**
+     * Retrieves the Filezilla binary handler instance.
+     * If the instance does not exist, it creates a new instance of BinFilezilla.
+     *
+     * @return BinFilezilla The Filezilla binary handler instance.
+     */
     public function getFilezilla()
     {
         if ($this->filezilla == null) {
@@ -129,6 +241,11 @@ class Bins
         return $this->filezilla;
     }
 
+    /**
+     * Retrieves the logs path for Filezilla.
+     *
+     * @return array An array containing the logs path for Filezilla.
+     */
     public function getLogsPath()
     {
         return array(
@@ -136,6 +253,11 @@ class Bins
         );
     }
 
+    /**
+     * Retrieves a list of all enabled services along with their respective service handlers.
+     *
+     * @return array An associative array where keys are service names and values are service instances.
+     */
     public function getServices()
     {
         $result = array();
