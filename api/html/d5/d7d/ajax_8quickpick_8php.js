5bef66362d38bd00fd2e71a5f4b13cd334254718
Bear <programming@hallhome.us>
2024-07-26 09:31:01 -0500

    Update classConfig.html: add new methods and constants; update project number and improve formatting

---
+++ api/html/d5/d7d/ajax_8quickpick_8php.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021-2024 Bearsampp
+ * License:  GNU General Public License version 3 or later; see LICENSE.txt
+ * Author: Bear
+ * Website: https://bearsampp.com
+ * Github: https://github.com/Bearsampp
+ */
+
+var ajax_8quickpick_8php =
+[
+    [ "$QuickPick", "d5/d7d/ajax_8quickpick_8php.html#a12b406bf41addc8510d212fbc623f95c", null ],
+    [ "$response", "d5/d7d/ajax_8quickpick_8php.html#af4b6fb1bbc77ccc05f10da3b16935b99", null ],
+    [ "else", "d5/d7d/ajax_8quickpick_8php.html#a95d082d22ba1864a6544af0da8ff0d96", null ]
+];
