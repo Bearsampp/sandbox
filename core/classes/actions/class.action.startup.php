fc3916da0aafdd310adaaecffaeea34276e0a34a
jwaisner <jwaisner212@gmail.com>
2026-04-02 22:45:15 -0500

    Replace round() with fmod() for seconds calculation in startup time formatting

--- core/classes/actions/class.action.startup.php
+++ core/classes/actions/class.action.startup.php
@@ -217,7 +217,7 @@ class ActionStartup
             global $bearsamppConfig;
             if ($bearsamppConfig->getLogsVerbose() == Config::VERBOSE_TRACE) {
                 $minutes = floor($startupTime / 60);  // floor() returns int-compatible value
-                $seconds = round($startupTime % 60, 2);
+                $seconds = fmod($startupTime, 60);
                 $formattedTime = sprintf('%d:%05.2f', $minutes, $seconds);
                 Util::logTrace('=== TOTAL STARTUP TIME: ' . $formattedTime . ' ===');
             }
